96dcb91bd70a7b871082dcce70b62f4fe7255de6
vaibhav raj <115108719+Heyvaibhav07@users.noreply.github.com>
2026-02-01 11:17:45 +0000

    JS Variable

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,21 @@
+const accountID=12345
+let accountEmail= "vaibhavraj@gmail.com"
+var accountPassword= "098765"
+accountCity="Bhagalpur"
+let accountState;
+
+// accountID = 2 // not allowed
+
+
+accountEmail="vr@gmail.com"
+accountPassword="987456"
+accountCity="Munger"
+
+console.log(accountID);
+
+/*
+Not to use VAR 
+becauase of issue ij block scope and functional scope
+*/
+
+console.table([accountID, accountEmail , accountPassword , accountCity, accountState])
