38128079f77f99f514e73ef0e0255fe437f88ce5
vaibhav raj <115108719+Heyvaibhav07@users.noreply.github.com>
2026-02-01 12:13:48 +0000

    operations

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,57 @@
+let score = "20abc"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "20" => 20
+// "20abc" => NaN
+// true => 1 , false => 0
+
+
+let isLoggedIn = 0
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true ,0 => false
+// "" => false
+// "Vaibhav" => true 
+
+
+let anyNumber = 10
+
+let stringNumber = String(anyNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+// ************************* OPERATIONS *********************
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " vaibhav"
+
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log((3 + 4) * 5 % 3);
+
